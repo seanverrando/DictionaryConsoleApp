9d00c90290030fcc8109d1162cc1081456ae219e
Sean <verrando19@gmail.com>
2021-10-01 00:02:45 +0900

    Add files via upload

---
+++ DictionaryProgram/DictionaryProgram/LookUpWord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+
+namespace DictionaryProgram
+{
+    class LookUpWord
+    {
+        //path the JSON file
+        JObject data = JObject.Parse(File.ReadAllText(@"C:\Users\seanv\source\repos\DictionaryProgram\data.json"));
+        public object ReturnDef(string word)
+        {
+            object term = new object();
+            word = word.ToLower();
+            if (data.ContainsKey(word))
+            {
+                var myWord = data[word];
+                // loop through the value deleting the [] and extra space to make it look nice
+                foreach(var value in myWord)
+                {
+                    //Console.WriteLine(item.Key);
+                    term = value;
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine( "Could not find word.");
+            }
+            return term;
+        }
+    }
+}
---
+++ DictionaryProgram/DictionaryProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+
+
+
+namespace DictionaryProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your dictionary");
+            LookUpWord dictionary = new LookUpWord();
+            Console.WriteLine("Enter a word: ");
+            string word = Console.ReadLine();
+            Console.WriteLine("");
+            object x = dictionary.ReturnDef(word);
+            Console.WriteLine(x);
+        }
+    }
+}
+    
+
+    
+
+   
